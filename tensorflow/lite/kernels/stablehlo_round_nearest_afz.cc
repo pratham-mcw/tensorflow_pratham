f209f9e6748b01aaff749eeb6369356f1df1ac05
pratham-mcw <pratham.kumar@multicorewareinc.com>
2024-08-29 12:54:27 +0530

    Reviewed changed

--- tensorflow/lite/kernels/stablehlo_round_nearest_afz.cc
+++ tensorflow/lite/kernels/stablehlo_round_nearest_afz.cc
@@ -12,6 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
+
 #include "tensorflow/lite/core/c/common.h"
 #include "tensorflow/lite/kernels/dequantize.h"
 #include "tensorflow/lite/kernels/internal/tensor.h"
